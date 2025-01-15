2c24e888fbc7931911bd3f7181eb64e8ea3c6630
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2025-01-15 07:10:59 -0300

    fix diff_settings.py for us only

--- diff_settings.py
+++ diff_settings.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python3
 
-def add_custom_arguments(parser):
-    group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument('-u', dest='lang', action='store_const', const='us',
-            help="Set version to US.")
-    group.add_argument('-eu10', dest='lang', action='store_const', const='eu-1.0',
-            help="Set version to EU.")
-    group.add_argument('-eu11', dest='lang', action='store_const', const='eu-final',
-            help="Set version to EU.")
-
 def apply(config, args):
-    lang = args.lang or 'us'
-    config['mapfile'] = f'build/{lang}/mk64.{lang}.map'
-    config['myimg'] = f'build/{lang}/mk64.{lang}.z64'
-    config['baseimg'] = f'baserom.{lang}.z64'
-    config['makeflags'] = [f'VERSION={lang}']
+    config['mapfile'] = f'build/us/mk64.us.map'
+    config['myimg'] = f'build/us/mk64.us.z64'
+    config['baseimg'] = f'baserom.us.z64'
+    config['makeflags'] = [f'VERSION=us']
     config['source_directories'] = ['.']
